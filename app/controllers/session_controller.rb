ca6e2a3830f3dcf2a5f2fa515565ff3990921fef
Madhanagopal <nmadhanagopal.2000@gmail.com>
2021-06-24 15:43:19 +0530

    Create session controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,20 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      redirect_to "/"
+    else
+      flash[:error] = "Your login attempt was invalid. Please retry."
+      redirect_to new_sessions_path
+    end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    @current_user = nil
+  end
+end
