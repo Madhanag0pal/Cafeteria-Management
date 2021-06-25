f8bb4cb94f7e2bf78f2ced973e966cb78a8d5516
Madhanagopal <nmadhanagopal.2000@gmail.com>
2021-06-25 17:15:58 +0530

    Update some controllers and views

--- db/schema.rb
+++ db/schema.rb
@@ -1,24 +1,25 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2021_06_21_124327) do
+ActiveRecord::Schema.define(version: 2021_06_24_072952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "name"
     t.string "address"
+  end
+
+  create_table "menu_categories", force: :cascade do |t|
+    t.string "name"
+    t.boolean "status"
+  end
+
+  create_table "menu_items", force: :cascade do |t|
+    t.bigint "menu_categorie_id"
+    t.string "name"
+    t.string "description"
+    t.float "price"
+    t.boolean "status"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -26,10 +27,9 @@ ActiveRecord::Schema.define(version: 2021_06_21_124327) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "role"
+    t.bigint "role_id"
     t.string "name"
     t.string "email"
     t.string "password_digest"
   end
-
 end
