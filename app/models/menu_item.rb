0f6e4fbdab6ce450dc92a9141ba7c0e3cd8ce027
Madhanagopal <nmadhanagopal.2000@gmail.com>
2021-06-27 20:27:13 +0530

    create cart items model

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,5 +1,6 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu_categorie
+  has_many :cart_items, dependent: :destroy
 
   validates :name, :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   belongs_to :role
   has_many :addresses, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
