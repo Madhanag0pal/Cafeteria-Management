9775db68d78976f27505e2a5f955d52d9044f51a
Madhanagopal <nmadhanagopal.2000@gmail.com>
2021-07-17 11:52:50 +0530

    Add chart kick gem and its dependencies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "bootstrap";
+import "chartkick/chart.js"
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 
